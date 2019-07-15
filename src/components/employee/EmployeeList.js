9605a14c0328495bf282bc9e0ec32fbd5649770f
Misty DeRamus <mistyderamus@gmail.com>
2019-07-15 14:32:50 -0500

    add delete button to employee

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import fire from "./EmployeeIcon.svg"
+// import "./Animal.css"
 
-export default class EmployeeList extends Component {
-  render() {
-    return (
-      <section className="employees">
-        {this.props.employees.map(employee => (
-          <div key={employee.id}>{employee.name}</div>
-        ))}
-      </section>
-    );
-  }
+export default class EmployeeLIst extends Component {
+    render () {
+        return (
+            <section className="employees">
+            {
+                this.props.employees.map(employee =>
+                    <div key={employee.id} className="card">
+                        <div className="card-body">
+                            <div className="card-title">
+                                <img src= {fire} alt ="fire" className="icon--fire" />
+                                <h5>{employee.name}</h5>
+                                <button
+                                    onClick={() => this.props.deleteEmployee(employee.id)}
+                                    className="card-link">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            </section>
+        )
+    }
 }
+
