b83a044a743f69bf28dce91554695ada522a8a25
Misty DeRamus <mistyderamus@gmail.com>
2019-07-17 10:35:41 -0500

    add modules and employee sign in

---
+++ src/components/authentication/Login.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react"
+
+
+export default class Login extends Component {
+
+    // Set initial state needs to be empty
+    state = {
+        email: "",
+        password: ""
+    }
+
+    // Update state whenever an input field is edited
+    handleFieldChange = (evt) => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    // Simplistic handler for login submit; e is event
+    handleLogin = (e) => {
+        e.preventDefault()
+
+        /*
+            For now, just store the email and password that
+            the customer enters into local storage.
+        */
+        sessionStorage.setItem(
+            "credentials",                       //key
+            JSON.stringify({
+                email: this.state.email,        //values coming from state
+                password: this.state.password
+            })
+        )
+    }
+//form of the form
+    render() {
+        return (
+            <form onSubmit={this.handleLogin}>
+                <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <label htmlFor="inputEmail">
+                    Email address
+                </label>
+                <input onChange={this.handleFieldChange} type="email"
+                       id="email"
+                       placeholder="Email address"
+                       required="" autoFocus="" />
+                <label htmlFor="inputPassword">
+                    Password
+                </label>
+                <input onChange={this.handleFieldChange} type="password"
+                       id="password"
+                       placeholder="Password"
+                       required="" />
+                <button type="submit">
+                    Sign in
+                </button>
+            </form>
+        )
+    }
+}
